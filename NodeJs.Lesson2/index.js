a916e9f521841cc465b48e053c74eeb1b9e4b3d7
Roman Gromov <GromRom1289@yandex.ru>
2022-05-20 21:05:36 +0300

    NodeJs.Lesson2

---
+++ NodeJs.Lesson2/index.js
@@ -0,0 +1,39 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+let moment = process.argv[2];
+class Handler {
+	static send() {
+		console.log(`Before timer elapsed: ${getTimeRemaining(moment).total / 1000}`)
+	}
+	static stop() {
+		console.log('Timer was elapsed');
+		clearInterval(timerID);
+	}
+}
+emitter.on("send", Handler.send);
+emitter.on("stop", Handler.stop);
+
+
+function getTimeRemaining(endtime) {
+	let t = Date.parse(endtime) - Date.parse(new Date());
+	let seconds = Math.floor((t / 1000) % 60);
+	let minutes = Math.floor((t / 1000 / 60) % 60);
+	let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	let days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+
+};
+let timerID = setInterval(() => {
+	if (getTimeRemaining(moment).total > 0) {
+		emitter.emit("send")
+	}
+	else {
+		emitter.emit("stop")
+	}
+}, 1000);
